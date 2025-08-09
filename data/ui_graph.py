b57548c35e99db6fe50820442c1bf6c4cc1042cb
shuuucq <83166594+shuuucq@users.noreply.github.com>
2025-08-09 16:51:27 +0800

    Update ui_graph.py

--- data/ui_graph.py
+++ data/ui_graph.py
@@ -21,9 +21,9 @@ class Interaction(Data, Graph):
         self.test_set = defaultdict(dict)
         self.test_set_item = set()
 
-        self.__generate_set()  # 生成训练集和测试集的用户-物品对
-        self.user_num = len(self.user)  # 用户总数
-        self.item_num = len(self.item)  # 物品总数
+        self.__generate_set() 
+        self.user_num = len(self.user) 
+        self.item_num = len(self.item) 
         self.ui_adj = self.__create_sparse_bipartite_adjacency()
         self.norm_adj = self.normalize_graph_mat(self.ui_adj)
         self.interaction_mat = self.__create_sparse_interaction_matrix()
@@ -54,9 +54,7 @@ class Interaction(Data, Graph):
             self.test_set[user][item] = 1
             self.test_set_item.add(item)
 
-            # if user in self.user and item in self.item:
-            #     self.test_set[user][item] = 1
-            #     self.test_set_item.add(item)
+
 
         for user, item in self.val_data:
             if user in self.user and item in self.item:
@@ -66,8 +64,8 @@ class Interaction(Data, Graph):
 
     def __create_sparse_bipartite_adjacency(self, self_connection=False):
         n_nodes = self.user_num + self.item_num
-        user_np = np.array([self.user[pair[0]] for pair in self.training_data])  # 使用映射后的用户索引
-        item_np = np.array([self.item[pair[1]] for pair in self.training_data]) + self.user_num  # 使用映射后的物品索引
+        user_np = np.array([self.user[pair[0]] for pair in self.training_data])  
+        item_np = np.array([self.item[pair[1]] for pair in self.training_data]) + self.user_num  
         ratings = np.ones_like(user_np, dtype=np.float32)
         tmp_adj = sp.csr_matrix((ratings, (user_np, item_np)), shape=(n_nodes, n_nodes), dtype=np.float32)
         adj_mat = tmp_adj + tmp_adj.T
@@ -120,13 +118,12 @@ class Interaction(Data, Graph):
     def item_rated(self, i):
         return list(self.training_set_i[i].keys()), list(self.training_set_i[i].values())
     def get_positive_item(self, u):
-        # 返回用户 u 的一个正样本物品的内部索引
-        # 这里简单地返回用户交互过的第一个物品
+
         history_item_ids, _ = self.user_rated(u)
         if len(history_item_ids) > 0:
             return history_item_ids[0]
         else:
-            # 如果没有正样本，随机返回一个物品索引
+
             return np.random.randint(self.item_num)
 
     def row(self, u):
